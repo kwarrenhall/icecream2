6e1409a2e8ca07e5904e1c1312be4b829a224c1c
kate hall and Ned Faulhaber <kate@>
2016-03-28 16:02:48 -0700

    add loop

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,10 @@
 $(document).ready(function() {
 
-  
+var flavors=["chocolate", "coffee", "silly"];
+flavors.forEach(function(flavor){
+// result location are names oddly based on madlibs example not a great solution
+  $("." + flavor).append(flavor);
+});
+
   event.preventDefault();
-  });
 });
